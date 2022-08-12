cc5188e6df207d161b9d0722c76162dabe05fad3
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2022-08-12 14:20:27 +0530

    start live chat app content

---
+++ src/LoginFormController.java
@@ -0,0 +1,11 @@
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
+public class LoginFormController {
+    public JFXTextField txtUserName;
+    
+
+    public void btnLogInOnAction(ActionEvent actionEvent) {
+        
+    }
+}
