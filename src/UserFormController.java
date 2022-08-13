c242d12c6d53d0ae02b83c37227761e494784e11
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2022-08-13 16:46:06 +0530

    updated userController initialize content

--- src/UserFormController.java
+++ src/UserFormController.java
@@ -4,12 +4,15 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 
-public class UserFormController implements Initializable {
+public class UserFormController {
     @FXML
     public TextField txtUserWriteMassage;
 
@@ -21,8 +24,21 @@ public class UserFormController implements Initializable {
 
     }
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    public void initialize(){
+        new Thread(()->{
+            try {
+                ServerSocket serverSocket = new ServerSocket(5000);
+                System.out.println("Server Started!");
+                accept = serverSocket.accept();
+                System.out.println("Client Connected!");
+                InputStreamReader inputStreamReader = new InputStreamReader(accept.getInputStream());
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String record = bufferedReader.readLine();
+                System.out.println(record);
 
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }).start();
     }
 }
