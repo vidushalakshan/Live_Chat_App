02b2a5ba5be219cfb322e9fdb0b42822d2bf235e
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2022-08-13 17:07:59 +0530

    updated userTwo initializer

---
+++ src/UserFormTwoController.java
@@ -0,0 +1,19 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.Socket;
+
+public class UserFormTwoController {
+    public AnchorPane root;
+    public TextField txtUserWriteMassageTwo;
+    public TextArea vboxShowMassageTwo;
+
+    Socket socket = null;
+
+
+    public void btnSendOnActionTwo(ActionEvent actionEvent) {
+        
+    }
+}
